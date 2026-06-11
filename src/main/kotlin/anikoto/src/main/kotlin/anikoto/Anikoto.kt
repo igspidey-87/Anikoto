a367a78e9842e74983f090a6cfee9bab7ce77418
igspidey-87 <ktm25102009@gmail.com>
2026-06-11 22:11:12 +0530

    Create Anikoto.kt

---
+++ src/main/kotlin/anikoto/src/main/kotlin/anikoto/Anikoto.kt
@@ -0,0 +1,12 @@
+package anikoto
+
+import com.lagradost.cloudstream3.*
+
+class Anikoto : MainAPI() {
+    override var name = "Anikoto"
+    override var mainUrl = "https://anikoto.cz"
+
+    override val supportedTypes = setOf(
+        TvType.Anime
+    )
+}
